7ffd62ec886690e1c18dd353a705c9fb57039556
omatsola619 <omarsunday0@gmail.com>
2023-09-17 09:22:54 +0100

    started rebranding boldie

--- screens/Home.js
+++ screens/Home.js
@@ -1,148 +1,173 @@
-import { Image, Pressable, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View, ScrollView } from 'react-native'
-import React, { useState } from 'react'
-import sally from '../assets/image/press.png'
-import {COLORS} from '../utils/color'
-import { LinearGradient } from 'expo-linear-gradient';
+import {
+  Image,
+  ImageBackground,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  ScrollView,
+} from "react-native";
+import React, { useState } from "react";
+import sally from "../assets/image/press.png";
+import { COLORS } from "../utils/color";
+import { LinearGradient } from "expo-linear-gradient";
+import BG from "../assets/image/grad3.png";
+import { AntDesign } from '@expo/vector-icons';
 
-const Home = ({navigation}) => {
-    const [mytext, setMytext] = useState('')
-    const [mycolor, setMyColor] = useState('#000000')
 
-    const handleText = (text) => {
-        setMytext(text)
+const Home = ({ navigation }) => {
+  const [mytext, setMytext] = useState("");
+  const [mycolor, setMyColor] = useState("#000000");
+  const majorColor = "#EC298C"
+
+  const handleText = (text) => {
+    setMytext(text);
+  };
+
+  const handleColor = (color) => {
+    setMyColor(color);
+  };
+
+  const handleSumbit = () => {
+    if (mytext !== "") {
+      const data = { mycolor, mytext };
+      navigation.navigate("Preview", data);
     }
-
-    const handleColor = (color) => {
-        setMyColor(color)
-    }
-
-    const handleSumbit = () => {
-        if(mytext !== ''){
-            const data = { mycolor, mytext };
-            navigation.navigate("Preview", data);
-        }
-    }
+  };
 
   return (
-    <SafeAreaView style={styles.container}>
-      <ScrollView style={styles.wrapper}>
-        <View style={styles.imageWrapper}>
-          <Image source={sally} style={styles.sally} />
-        </View>
-
-        {/* form field  */}
-        <View style={styles.form}>
-          <Text style={styles.intro}>Write for Me Boldie</Text>
-          <View style={styles.formField}>
-            <TextInput
-              placeholder="Input Text"
-              style={styles.input}
-              value={mytext}
-              onChangeText={handleText}
-            />
-          </View>
-
-          {/* color picker component  */}
-          <View style={styles.colorPicker}>
-            <View style={styles.colorsArr}>
-              {COLORS.map((item, i) => (
-                <TouchableOpacity key={i} onPress={() => handleColor(item.code)}>
-                  <View
-                    style={{
-                      backgroundColor: item.code,
-                      width: 35,
-                      height: 35,
-                      aspectRatio: 1,
-                      margin: 2,
-                      borderRadius: 5,
-                      // borderWidth: mycolor === item.code && 3,
-                      borderColor: mycolor === item.code && '#5452ed',
-                    }}
-                  ></View>
-                </TouchableOpacity>
-              ))}
+    <ImageBackground
+      source={BG}
+      resizeMode="cover"
+      style={{
+        flex: 1,
+        justifyContent: "center",
+      }}
+    >
+      <SafeAreaView style={styles.container}>
+        <ScrollView style={styles.wrapper}>
+          {/* form field  */}
+          <View style={styles.form}>
+            <Text style={styles.intro}>Write for Me <Text style={{color: majorColor}}>Boldie</Text> </Text>
+            <View style={styles.formField}>
+              <TextInput
+                placeholder="Input Text"
+                style={styles.input}
+                value={mytext}
+                onChangeText={handleText}
+              />
             </View>
+
+            {/* color picker component  */}
+            <View style={styles.colorPicker}>
+              <View style={styles.colorsArr}>
+                {COLORS.map((item, i) => (
+                  <TouchableOpacity
+                    key={i}
+                    onPress={() => handleColor(item.code)}
+                  >
+                    <View
+                      style={{
+                        backgroundColor: item.code,
+                        width: 35,
+                        height: 35,
+                        aspectRatio: 1,
+                        margin: 2,
+                        borderRadius: 5,
+                        // borderWidth: mycolor === item.code && 3,
+                        borderColor: mycolor === item.code && "#5452ed",
+                      }}
+                    ></View>
+                  </TouchableOpacity>
+                ))}
+              </View>
+            </View>
+
+            {/* submit button  */}
+            <View style={styles.submitWrapper}>
+              <TouchableOpacity
+              onPress={handleSumbit}
+              activeOpacity={0.8}
+              style={styles.submit}
+
+            >
+              <AntDesign name="arrowright" size={25} color={majorColor} />
+            </TouchableOpacity>
+            </View>
+            
           </View>
-
-          {/* submit button  */}
-          <TouchableOpacity onPress={handleSumbit} activeOpacity={0.7} style={styles.submit}>
-            <Text style={styles.submitTxt}>Preview</Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+        </ScrollView>
+      </SafeAreaView>
+    </ImageBackground>
   );
-}
+};
 
-export default Home
+export default Home;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#5452ed'
-    },
-    wrapper: {
-        flex: 1,
-        paddingHorizontal: 16,
-    },
-    txt: {
-        fontFamily: 'Poppins_500'
-    },
-    sally: {
-        width: 300,
-        height: 300
-    },
-    imageWrapper: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '100%',
-        marginTop: 50
-        
-    },
-    form: {
-        marginTop: 30
-    },
-    intro: {
-        textAlign: 'center',
-        marginBottom: 30,
-        fontSize: 40,
-        fontFamily: 'Zeyada',
-        color: 'white'
-    },
-    input: {
-        width: '100%',
-        height: 50,
-        backgroundColor: '#f2f2f2',
-        borderRadius: 8,
-        paddingLeft: 15
-    },
-    formField: {
-        marginBottom: 20
-    },
-    colorPicker: {
-        width: '100%',
-        height: 140,
-        backgroundColor: 'white',
-        borderRadius: 8
-    },
-    colorsArr: {
-        flexWrap: 'wrap',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        padding: 10,
-    },
-    submit: {
-        marginTop: 20,
-        width: '100%',
-        backgroundColor: 'white',
-        height: 50,
-        borderRadius: 8,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    submitTxt: {
-        fontFamily: 'Poppins_700',
-        fontSize: 18,
-        color: 'grey'
-    }
-})
+  container: {
+    flex: 1,
+    // backgroundColor: '#5452ed'
+  },
+  wrapper: {
+    flex: 1,
+    paddingHorizontal: 16,
+  },
+  txt: {
+    fontFamily: "Poppins_500",
+  },
+  form: {
+    marginTop: 30,
+  },
+  intro: {
+    textAlign: "center",
+    marginTop: 80,
+    marginBottom: 30,
+    fontSize: 40,
+    fontFamily: "Roboto_700",
+    color: "white",
+  },
+  input: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#f2f2f2",
+    borderRadius: 8,
+    paddingLeft: 15,
+    fontSize: 25
+  },
+  formField: {
+    marginBottom: 20,
+  },
+  colorPicker: {
+    width: "100%",
+    height: 250,
+    marginTop: 40,
+    backgroundColor: "white",
+    borderRadius: 8,
+  },
+  colorsArr: {
+    flexWrap: "wrap",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 10,
+  },
+  submitWrapper: {
+    marginTop: 40,
+    alignItems: 'center',
+  },
+  submit: {
+    width: 70,
+    backgroundColor: "white",
+    height: 70,
+    borderRadius: 50,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  submitTxt: {
+    fontFamily: "Poppins_700",
+    fontSize: 18,
+    color: "grey",
+  },
+});
--- utils/font.js
+++ utils/font.js
@@ -10,6 +10,12 @@ import {
     Poppins_900Black
 } from '@expo-google-fonts/poppins';
 import { Zeyada_400Regular } from '@expo-google-fonts/zeyada';
+import { 
+    RobotoCondensed_300Light,
+    RobotoCondensed_400Regular, 
+    RobotoCondensed_700Bold,
+} from '@expo-google-fonts/roboto-condensed';
+
 
 
 export const useCustomFonts = () => {
@@ -23,6 +29,9 @@ export const useCustomFonts = () => {
         'Poppins_800': Poppins_800ExtraBold,
         'Poppins_900': Poppins_900Black,
         'Zeyada': Zeyada_400Regular,
+        'Roboto_300': RobotoCondensed_300Light,
+        'Roboto_400': RobotoCondensed_400Regular,
+        'Roboto_700': RobotoCondensed_700Bold,
       });
 
   return fontsLoaded;
